4b592dd108720465d6881f6f0b2e7476b249fcc1
a-cole55 <arniqua_c@yahoo.com>
2022-05-27 07:54:09 -0500

    added animation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import Text from './components/Text'
-import vid from './assets/pexels-cottonbro.mp4'
+import vid from './assets/pexels-cottonbro.mp4';
+import Text from './components/Text';
 
 
 export default function App() {
   return (
     <div className="App">
       <div className='Video'>
-      <video width="600" autoPlay muted loop>
-      <source src={vid} type="video/mp4" />
-        </video>
+          <video width="600" autoPlay muted loop>
+            <source src={vid} type="video/mp4" />
+          </video>
       </div>
       <Text />
     </div>
--- src/components/Text.js
+++ src/components/Text.js
@@ -1,10 +1,19 @@
+// import { Component } from "react";
+import styled, { keyframes } from "styled-components";
+import { bounceInRight } from "react-animations";
+const BounceInRightAnimation = keyframes`${bounceInRight}`;
+const BounceInRightDiv = styled.div`
+  animation: 5s ${BounceInRightAnimation};
+`;
+
+
 
 export default function Text(){
     return(
         <div className="Text">
             <div className="mainText">
                 <h2>Fenty Apparel</h2>
-                <h1>Coming Soon</h1>
+                <BounceInRightDiv><h1>Coming Soon</h1></BounceInRightDiv>
                 <p>If you would like to be notified when our site launches</p>
                 <button>Click Here</button>
             </div>
